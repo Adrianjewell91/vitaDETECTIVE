87e5bcb2aed6209dffa64d2f329c3d23147420e5
Adrian <adrianjewell91@gmail.com>
2017-12-09 13:04:33 -0800

    first

---
+++ README.md
@@ -0,0 +1,36 @@
+## `express`
+
+## How to run
+
+### Register your app
+
+Visit "My apps" console and set
+
+- Name: as you like
+- Redirect uris: `http://127.0.0.1:3000/callback`
+
+Set scopes (whitelists) in "Authorization scopes" panel.
+
+- [x] report:eye-color
+- [x] report:beard-thickness
+- [x] report:morning-person
+
+### Run your app
+
+```
+$ npm install
+$ export GENOMELINK_CLIENT_ID=<your_client_id>
+$ export GENOMELINK_CLIENT_SECRET=<your_client_secret>
+$ export GENOMELINK_CALLBACK_URL="http://127.0.0.1:3000/callback"
+$ node app.js
+```
+
+then, visit `http://127.0.0.1:3000`
+
+## How it works
+
+See https://github.com/AWAKENS-dev/api-oauth-example-flask/issues/1
+
+## Requirements
+
+Node >= 8.9.0
---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const session = require('express-session');
+const genomeLink = require('genomelink-node');
+
+const vitamin_list = require('./vitamin_qualities.js');
+
+// export GENOMELINK_CLIENT_ID=t0pRdHSsViMvhmFKGejrph0jvtyQFx760cz32qKB
+// export GENOMELINK_CLIENT_SECRET=gi27X7FmYpqv0dkb5VJTsuBoNpOG7uBjDFxvdLg1uE3Aqj2UE9vKtWZI24bcJIdfrjFYRRu6AM5qV6OuWZ3HYSg33l08ONAPD6TnH2IxMoiA3IEm35Q2DdyMoxdsDlos
+// export GENOMELINK_CALLBACK_URL="http://127.0.0.1:3000/callback"
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// Use the session middleware
+
+app.use(session({
+  secret: 'YOURSECRET',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 30 * 60 * 1000
+  }
+}));
+
+app.get('/', async (req, res) => {
+  const scope = 'report:folate report:iron report:calcium report:blood-glucose';
+  const authorizeUrl = genomeLink.OAuth.authorizeUrl({ scope: vitamin_list.join(' ') });
+
+  // Fetching a protected resource using an OAuth2 token if exists.
+  let reports = [];
+
+  // console.log(req.session.oauthToken);
+  console.log(vitamin_list);
+  if (req.session.oauthToken) {
+    const scopes = vitamin_list;
+    reports = await Promise.all(scopes.map( async (name) => {
+      return await genomeLink.Report.fetch({
+        name: name.replace(/report:/g, ''),
+        population: 'european',
+        token: req.session.oauthToken
+      })
+    }));
+  }
+
+  res.render('index', {
+    authorize_url: authorizeUrl,
+    reports: reports,
+  });
+});
+
+app.get('/callback', async (req, res) => {
+  // The user has been redirected back from the provider to your registered
+  // callback URL. With this redirection comes an authorization code included
+  // in the request URL. We will use that to obtain an access token.
+  req.session.oauthToken = await genomeLink.OAuth.token({ requestUrl: req.url });
+  console.log(req.url);
+  console.log(req.session.oauthToken);
+
+  // At this point you can fetch protected resources but lets save
+  // the token and show how this is done from a persisted token in index page.
+  res.redirect('/');
+});
+
+// Run local server on port 3000.
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function () {
+  console.log('Server running at http://127.0.0.1:' + port + '/');
+});
