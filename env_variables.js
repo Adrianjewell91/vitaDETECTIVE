6dfb46bb27ff3dba0fe66d55a145069a78d1ab0b
Adrian <adrianjewell91@gmail.com>
2017-12-10 15:28:56 -0800

    heroku url change

--- env_variables.js
+++ env_variables.js
@@ -1,6 +1,6 @@
 const VARIABLES = { GENOMELINK_CLIENT_ID: 'WifvK3NwuIDkpeNyYwqK5PGbAEVQMQmbrQ2H15A9',
                     GENOMELINK_CLIENT_SECRET: 'trRKV47faAFqrN2kpFlu4xn87d3pnZMQ6UttfxvMjOh4O3BEzjub18a10hRsaZ5E5gzWztmT7ZxxmEgZFymNWo48PXQSrrRIAiNd6o9jLj8I9Qmg0lTucJYyqtmW0FX8',
-                    GENOMELINK_CALLBACK_URL: 'http://127.0.0.1:3000/callback',
+                    GENOMELINK_CALLBACK_URL: 'https://vitadetective-api.herokuapp.com/callback',
 
                     AWS_1: 'AKIAIV5LBVLYWEEHDY6Q',
                     AWS_2: 'AHU68vB//Ask3AgqyWWbPoagus5oBvFFTciRopnz',
