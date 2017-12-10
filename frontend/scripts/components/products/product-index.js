a2013290548c8ac2d3de8bf5e3355cb2bfcb46ef
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-12-09 22:05:31 -0800

    created product index component

---
+++ frontend/scripts/components/products/product-index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProductIndex extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>I am the Product Index<h1>
+            </div>
+        );
+    }
+}
