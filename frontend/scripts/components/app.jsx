3a9d47e9cd926682e2291568a2d51cd8ddd6a55a
Cory Kane <ckane30691@gmail.com>
2017-12-09 21:42:54 -0800

    Setup react & redux

---
+++ frontend/scripts/components/app.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+const App = () => (
+    <div>
+        <header>
+            <div>
+                'React is great'
+            </div>
+        </header>
+
+
+
+        <Switch>
+
+        </Switch>
+    </div>
+);
+
+export default App;
