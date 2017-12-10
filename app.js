2ba94d0ad601e08158d38cd047e1136a82f14cf3
Adrian <adrianjewell91@gmail.com>
2017-12-09 20:17:43 -0800

    update

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const genomeLink = require('genomelink-node');
 const path = require('path')
 const vitamin_list = require('./vitamin_qualities.js');
 
-const GENOMELINK_CLIENT_ID='t0pRdHSsViMvhmFKGejrph0jvtyQFx760cz32qKB';
-const GENOMELINK_CLIENT_SECRET='gi27X7FmYpqv0dkb5VJTsuBoNpOG7uBjDFxvdLg1uE3Aqj2UE9vKtWZI24bcJIdfrjFYRRu6AM5qV6OuWZ3HYSg33l08ONAPD6TnH2IxMoiA3IEm35Q2DdyMoxdsDlos';
-const GENOMELINK_CALLBACK_URL='https://vitadetective-api.herokuapp.com/callback';
+// const GENOMELINK_CLIENT_ID='t0pRdHSsViMvhmFKGejrph0jvtyQFx760cz32qKB';
+// const GENOMELINK_CLIENT_SECRET='gi27X7FmYpqv0dkb5VJTsuBoNpOG7uBjDFxvdLg1uE3Aqj2UE9vKtWZI24bcJIdfrjFYRRu6AM5qV6OuWZ3HYSg33l08ONAPD6TnH2IxMoiA3IEm35Q2DdyMoxdsDlos';
+// const GENOMELINK_CALLBACK_URL='https://vitadetective-api.herokuapp.com/callback';
 
 const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 // Use the session middleware
 app.use(express.static('frontend'));
+
 app.use(session({
   secret: 'YOURSECRET',
   resave: false,
@@ -24,16 +26,33 @@ app.use(session({
 }));
 
 app.get('/', async (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/index.html'));
+  // res.sendFile(path.join(__dirname, '/frontend/index.html'));
+  const GENOMELINK_CLIENT_ID='t0pRdHSsViMvhmFKGejrph0jvtyQFx760cz32qKB';
+  const GENOMELINK_CLIENT_SECRET='gi27X7FmYpqv0dkb5VJTsuBoNpOG7uBjDFxvdLg1uE3Aqj2UE9vKtWZI24bcJIdfrjFYRRu6AM5qV6OuWZ3HYSg33l08ONAPD6TnH2IxMoiA3IEm35Q2DdyMoxdsDlos';
+  const GENOMELINK_CALLBACK_URL='http://127.0.0.1:3000/callback';
+
+  const authorizeUrl = await genomeLink.OAuth.authorizeUrl({scope: vitamin_list.join(' '), clientId: GENOMELINK_CLIENT_ID, callbackUrl: GENOMELINK_CALLBACK_URL});
+
+  res.render('index', {
+    authorize_url: authorizeUrl,
+  });
+
 });
 
 app.get('/callback', async (req, res) => {
   // The user has been redirected back from the provider to your registered
   // callback URL. With this redirection comes an authorization code included
   // in the request URL. We will use that to obtain an access token.
-  let reports = [];
 
   req.session.oauthToken = await genomeLink.OAuth.token({ requestUrl: req.url })
+
+
+  res.redirect('/single_page');
+});
+
+app.get('/single_page', async (req,res) => {
+
+  let reports = [];
 
   if (req.session.oauthToken) {
     const scopes = vitamin_list;
@@ -47,6 +66,7 @@ app.get('/callback', async (req, res) => {
   }
 
   res.json(reports);
+
 });
 
 // Run local server on port 3000.
