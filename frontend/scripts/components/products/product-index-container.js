478d85754eda34e2b5a391d6112b93388d73d87c
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-12-10 10:11:13 -0800

    added Product Index Container

--- frontend/scripts/components/products/product-index-container.js
+++ frontend/scripts/components/products/product-index-container.js
@@ -1,1 +1,12 @@
 import { connect } from 'react-redux';
+import ProductIndex from './product-index';
+
+const mapStateToProps = (state) => ({ 
+    products: state.products 
+});
+
+const mapDispatchToProps = (dispatch) => ({ 
+    fetchProducts: () => dispatch(fetchProducts)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductIndex);
