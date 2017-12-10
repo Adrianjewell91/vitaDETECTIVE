2b5b73d139e87f59b87760d6f1e23a2241524952
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-12-10 10:35:15 -0800

    added product actions

---
+++ frontend/scripts/actions/product_actions.js
@@ -0,0 +1,24 @@
+export const RECEIVE_PRODUCTS = "RECEIVE_PRODUCTS";
+
+// utils
+export const getProducts = (vitamin) => {
+    return $.ajax({
+      method: 'GET',
+      url: '/aws',
+      data: { vitamin }
+    });
+}
+
+// action-creators
+
+export const receiveProducts = (products) => ({
+  type: RECIEVE_PRODUCTS
+  products
+})
+
+//thunks
+
+export const requestProducts = (vitamin) => (dispatch) => {
+  return getProducts(vitamin)
+    .then((products) => dispatch(receiveProducts(products)))
+}
